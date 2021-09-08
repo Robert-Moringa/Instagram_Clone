4a04a908551700646ed40e0f81b80d4a36dab908
Robert Maina <robert.maina@student.moringaschool.com>
2021-09-09 00:30:44 +0300

    used bootstrap5, added styles

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap5',
     'instagram_clone.apps.InstagramCloneConfig',
     'django.contrib.admin',
     'django.contrib.auth',
