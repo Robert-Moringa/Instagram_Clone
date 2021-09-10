b2465d2f5f07c2ddd07f804c9a8187f0bb2ebf04
Robert Maina <robert.maina@student.moringaschool.com>
2021-09-10 05:43:20 +0300

    added the image model

--- instagram_clone/models.py
+++ instagram_clone/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length = 60)
+    image_path = models.ImageField(upload_to = 'uploads/')
+    caption = models.TextField()
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    likes = models.ForeignKey('Likes', on_delete=models.CASCADE)
+    comments=models.ForeignKey('Comments', on_delete=models.CASCADE)
+
